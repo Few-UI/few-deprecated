01bdc0f203d7be9f391aee1f819922ecd8a6f1ed
pengw <wenjia.peng@siemens.com>
2020-05-06 01:18:13 +0000

    setup ts

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+![build](https://github.com/Few-UI/few-sample/workflows/build/badge.svg)
 Low code experiment
--- jest.config.js
+++ jest.config.js
@@ -1,52 +1,15 @@
-/* eslint-env amd, es6, node */
 module.exports = {
-    verbose: true,
-    testMatch: [
-        '**/test/*.test.js',
-        '**/__tests__/**/*.[jt]s?(x), **/?(*.)+(spec|test).[tj]s?(x)'
-    ],
-    /*
-    moduleNameMapper: {
-        '^afx-next/src/(.*)$': '<rootDir>/src/$1'
-    },
-    */
-    collectCoverageFrom: [
-      '**/*.{js,jsx}'
-    ],
-    coveragePathIgnorePatterns: [
-      '<rootDir>[/\\\\](coverage|dist|docs|flow-typed|node_modules|config|test)[/\\\\]',
-      'jest.config.js',
-      'rollup.config.js'
-    ],
-    setupFiles: [
-      // '<rootDir>/config/jest/polyfills.js'
-      // npm install jest babel-jest babel-polyfill mutationobserver-shim document-register-element --save-dev
-      // '<rootDir>/node_modules/babel-polyfill/dist/polyfill.js'
-      // '<rootDir>/node_modules/mutationobserver-shim/dist/mutationobserver.min.js',
-      // '<rootDir>/node_modules/document-register-element/build/document-register-element.node.js'
-    ],
-    /*
-    setupTestFrameworkScriptFile: '<rootDir>/index-specrunner.js',
-    */
-    testPathIgnorePatterns: [
-      '<rootDir>[/\\\\](coverage|dist|docs|node_modules|config)[/\\\\]'
-    ],
-    // testURL: 'http://localhost',
+    // roots: [ '<rootDir>' ],
     transform: {
-      '^.+\\.(js|jsx)$': [ 'babel-jest', {
-          presets: [
-              [ '@babel/preset-env', {
-                  targets: {
-                      node: 'current'
-                  }
-              } ]
-          ]
-      } ]
-      // '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
-      // '^(?!.*\\.(js|jsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js'
+        '^.+\\.tsx?$': 'ts-jest',
     },
-    testEnvironment: 'node',
-    transformIgnorePatterns: [
-      '[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'
-    ]
-};
+    // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    testMatch: [
+        '**/*.test.[tj]s?(x)'
+        // '**/__tests__/**/*.[jt]s?(x), **/?(*.)+(spec|test).[tj]s?(x)'
+    ],
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx,ts,tsx}'
+    ],
+    moduleFileExtensions: [ 'ts', 'tsx', 'js', 'jsx', 'json', 'node' ],
+}
